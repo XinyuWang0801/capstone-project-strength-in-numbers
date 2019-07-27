91648a18501c535ad7640ca2169618d495454f54
scientistcoco <courtney.lum@gmail.com>
2019-07-27 10:23:02 +1000

    Enable scripts

--- .travisDeploy.sh
+++ .travisDeploy.sh
@@ -1,12 +1,8 @@
+#!/bin/sh
+set -e
 
-# #!/bin/sh
-# set -e
-
-# # setup ssh-agent and provide the GitHub deploy key
-# eval "$(ssh-agent -s)"
-# openssl aes-256-cbc -K $encrypted_dc4c0df7bb33_key -iv $encrypted_dc4c0df7bb33_iv -in id_ed25519.enc -out id_ed25519 -d
-# chmod 600 id_ed25519
-# ssh-add id_ed25519
-
-# # commit the assets in build/ to the gh-pages branch and push to GitHub using SSH
-# ./node_modules/.bin/gh-pages -d build/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
+# setup ssh-agent and provide the GitHub deploy key
+eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_dc4c0df7bb33_key -iv $encrypted_dc4c0df7bb33_iv -in id_ed25519.enc -out id_ed25519 -d
+chmod 600 id_ed25519
+ssh-add id_ed25519
