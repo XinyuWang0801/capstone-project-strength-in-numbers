20ccc182257f5b7154e4e12b90ee1edfd47ff7bc
Yathu Arulnanthy <yathu@outlook.com>
2019-07-29 17:12:48 +1000

    error fix -  password input

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -68,7 +68,7 @@ class LoginPage extends React.Component {
           {usernameError && <Alert variant='danger' className="LoginPage__Form__Alert">Username cannot be empty</Alert>}
           <LoginTextbox id="username" className={usernameError ? "LoginTextbox__Fail":"LoginTextbox"} label="Email" exampleLabel="email@example.com" type="email" value={username} onChange={(e) => this.onChangeInput(e)} />
           {passwordError && <Alert variant='danger' className="LoginPage__Form__Alert">Password cannot be empty</Alert>}
-          <LoginTextbox id="password" className={usernameError ? "LoginTextbox__Fail":"LoginTextbox"} label="Password" exampleLabel="Hopefully something secure" type="password" value={password} onChange={(e) => this.onChangeInput(e)} />
+          <LoginTextbox id="password" className={passwordError ? "LoginTextbox__Fail":"LoginTextbox"} label="Password" exampleLabel="Hopefully something secure" type="password" value={password} onChange={(e) => this.onChangeInput(e)} />
           <div className="LoginPage__Form__Buttons">
             <Button onClick={this.handleClick}>
               <p className="Button__TextLogIn">Log In</p>
