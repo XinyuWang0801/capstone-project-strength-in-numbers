0bddec7e0c4c208a8ebe4e9a3f569cff82e6ac35
ScientistCoco <30433825+ScientistCoco@users.noreply.github.com>
2019-07-31 23:04:12 +1000

    Feature: account info and booking (#34)

--- src/store/actions/Actions.AccommodationBooking.js
+++ src/store/actions/Actions.AccommodationBooking.js
@@ -4,10 +4,13 @@ export const ACCOMMODATION_BOOKED = 'ACCOMMODATION_BOOKED';
 
 export const bookAccommodation = (dates, guests) => {
   return async (dispatch, getState) => {
-    const { exploreState: { accommodationInfo: { id } } } = getState();
+    const { exploreState: { accommodationInfo: { id } }, accountState } = getState();
 
     await Services.bookAccommodation(id, dates, guests);
+
     const nDates = Services.convertArrayOfDatesToObjects(dates);
+    await Services.bookAccommodationForUser(id, accountState.id, nDates);
+
     dispatch({ type: ACCOMMODATION_BOOKED, payload: nDates });
   };
 };
--- src/store/services/Service.Account.js
+++ src/store/services/Service.Account.js
@@ -6,3 +6,34 @@ export const createUser = async (user) => {
   const key = await userRef.push(user).getKey();
   return key;
 };
+
+export const getBookedAccommodationsIds = async (user) => {
+  let accommodationIds = null; // contains ids of the accommodations the user has booked
+
+  await database.ref('/bookings')
+    .orderByChild('userId')
+    .equalTo(user)
+    .once('value', (snapshot) => {
+      accommodationIds = snapshot.val();
+    });
+
+  return accommodationIds;
+};
+
+export const getBookedAccommodationDetails = async (accommodationIds) => {
+  const accommodations = [];
+
+  for (const accommodation of Object.values(accommodationIds)) {
+    await database.ref(`/accommodations/${accommodation.accommodationId}`)
+      .once('value', (snapshot) => {
+        const dates = Object.values(accommodation.dates);
+        const details = {
+          ...snapshot.val(),
+          bookedDates: [dates[0], dates[dates.length - 1]],
+        };
+        accommodations.push(details);
+      });
+  }
+
+  return accommodations;
+};
