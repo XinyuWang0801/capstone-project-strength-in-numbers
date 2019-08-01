932564f32200c3433b7fd3f87d65f69ffb286dac
ScientistCoco <30433825+ScientistCoco@users.noreply.github.com>
2019-08-01 23:27:33 +1000

    Feature: Login connect with db (#35)

--- src/store/actions/Actions.Account.js
+++ src/store/actions/Actions.Account.js
@@ -2,6 +2,8 @@ import * as Services from '../services';
 
 export const USER_CREATED = 'USER_CREATED';
 export const BOOKED_ACCOMMODATIONS_RETRIEVED = 'BOOKED_ACCOMMODATIONS_RETRIEVED';
+export const USER_LOGIN_FAILED = 'USER_LOGIN_FAILED';
+export const USER_LOGIN_SUCCEDED = 'USER_LOGIN_SUCCEDED';
 
 export const createUser = (user) => {
   return async (dispatch) => {
@@ -18,5 +20,17 @@ export const getBookedAccommodations = () => {
     const accommodations = await Services.getBookedAccommodationDetails(bookingIds);
 
     dispatch({ type: BOOKED_ACCOMMODATIONS_RETRIEVED, payload: accommodations });
+  };
+};
+
+export const verifyUser = (user) => {
+  return async (dispatch) => {
+    const res = await Services.verifyUser(user);
+    if (res) {
+      dispatch({ type: USER_LOGIN_SUCCEDED, payload: res });
+      return true;
+    }
+    dispatch({ type: USER_LOGIN_FAILED });
+    return false;
   };
 };
--- src/store/services/Service.Account.js
+++ src/store/services/Service.Account.js
@@ -1,26 +1,39 @@
 import { database } from '../../firebase';
 
+const rounds = 10;
+const bcrypt = require('bcryptjs');
+
 export const createUser = async (user) => {
+  const nPassword = bcrypt.hashSync(user.password, bcrypt.genSaltSync(rounds));
+  const nUser = {
+    ...user,
+    password: nPassword,
+  };
+
   const userRef = await database.ref('/users');
 
-  const key = await userRef.push(user).getKey();
+  const key = await userRef.push(nUser).getKey();
   return key;
 };
 
 export const getBookedAccommodationsIds = async (user) => {
-  let accommodationIds = null; // contains ids of the accommodations the user has booked
+  let accommodationIds = []; // contains ids of the accommodations the user has booked
 
   await database.ref('/bookings')
     .orderByChild('userId')
     .equalTo(user)
     .once('value', (snapshot) => {
-      accommodationIds = snapshot.val();
+      if (snapshot.val()) {
+        accommodationIds = snapshot.val();
+      }
     });
 
   return accommodationIds;
 };
 
 export const getBookedAccommodationDetails = async (accommodationIds) => {
+  if (accommodationIds.length === 0) { return []; }
+
   const accommodations = [];
 
   for (const accommodation of Object.values(accommodationIds)) {
@@ -36,4 +49,19 @@ export const getBookedAccommodationDetails = async (accommodationIds) => {
   }
 
   return accommodations;
+};
+
+export const verifyUser = async (user) => {
+  let res = false;
+  let userId = null;
+  await database.ref('/users')
+    .orderByChild('email')
+    .equalTo(user.username)
+    .once('value', (snapshot) => {
+      if (snapshot.val()) {
+        res = bcrypt.compareSync(user.password, Object.values(snapshot.val())[0].password);
+        userId = res ? Object.keys(snapshot.val())[0] : null;
+      }
+    });
+  return res ? userId : '';
 };
