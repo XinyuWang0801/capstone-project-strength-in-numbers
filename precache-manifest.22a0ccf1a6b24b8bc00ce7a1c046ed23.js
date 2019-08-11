04d5ada10992fc51b3925be9daea7d54d3ea1330
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-11 10:30:06 +0000

    Deploy unsw-cse-comp3900-9900/capstone-project-strength-in-numbers to github.com/unsw-cse-comp3900-9900/capstone-project-strength-in-numbers.git:gh-pages

---
+++ precache-manifest.22a0ccf1a6b24b8bc00ce7a1c046ed23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa5547d6af25e9cedeb1087060944e0",
+    "url": "/capstone-project-strength-in-numbers/index.html"
+  },
+  {
+    "revision": "9e990fef0a2acf46a87e",
+    "url": "/capstone-project-strength-in-numbers/static/css/2.260a819a.chunk.css"
+  },
+  {
+    "revision": "f87e19c30648c840ed4f",
+    "url": "/capstone-project-strength-in-numbers/static/css/main.c45c9b4a.chunk.css"
+  },
+  {
+    "revision": "9e990fef0a2acf46a87e",
+    "url": "/capstone-project-strength-in-numbers/static/js/2.e7c238cf.chunk.js"
+  },
+  {
+    "revision": "f87e19c30648c840ed4f",
+    "url": "/capstone-project-strength-in-numbers/static/js/main.78c268d6.chunk.js"
+  },
+  {
+    "revision": "b00bfb74e5abf5884f69",
+    "url": "/capstone-project-strength-in-numbers/static/js/runtime~main.8b453bb7.js"
+  }
+]);
